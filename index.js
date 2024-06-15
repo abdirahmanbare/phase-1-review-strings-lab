dc45a4d3bf778bbfe668f04198a56003d1df4056
abdirahmanbare <abdirahmanhusseinbare@gmail.com>
2024-06-15 10:05:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+const currentUser =(" Welcome to Flatbook, currentUser")
+const welcomeMessage1 = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage0 = (`Welcome to Flatbook, ' ${currentUser}`);
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
